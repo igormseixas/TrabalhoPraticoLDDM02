c28b5bba1f9bcbcd37721ea0773c0a328af5c676
Igor Seixas <igormseixas@hotmail.com>
2017-11-06 22:39:09 -0200

    Fragment Handle

--- app/src/main/java/com/lddmproject/trabalhopraticolddm02/MainActivity.java
+++ app/src/main/java/com/lddmproject/trabalhopraticolddm02/MainActivity.java
@@ -3,6 +3,7 @@ package com.lddmproject.trabalhopraticolddm02;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -78,14 +79,24 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        int id = item.getItemId( );
+        FragmentManager fragmentManager = getFragmentManager();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
+        if (id == R.id.nav_first_layout) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame
+                            , new FirstFragment( ))
+                    .commit();
+        } else if (id == R.id.nav_second_layout) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame
+                            , new SecondFragment( ))
+                    .commit();
+        } else if (id == R.id.nav_third_layout) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame
+                            , new ThirdFragment( ))
+                    .commit();
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
